1cfa4ab3603ab1ce7aa16eb64caa24a09aa12a66
fpirchner <florian.pirchner@gmail.com>
2011-12-14 10:32:18 +0100

    Demo

--- demo/com/spaceapplications/vaadin/addon/eventtimeline/demo/SimpleEventTimelineDemo.java
+++ demo/com/spaceapplications/vaadin/addon/eventtimeline/demo/SimpleEventTimelineDemo.java
@@ -11,59 +11,89 @@ import com.vaadin.Application;
 import com.vaadin.ui.Window;
 
 public class SimpleEventTimelineDemo extends Application {
-  @Override
-  public void init() {
-    Window mainWindow = new Window("Simple EventTimeline Demo");
+	@Override
+	public void init() {
+		Window mainWindow = new Window("Simple EventTimeline Demo");
 
-    setMainWindow(mainWindow);
+		setMainWindow(mainWindow);
 
-    // create the timeline
-    EventTimeline timeline = new EventTimeline("Our event timeline");
-    timeline.setHeight("500px");
-    timeline.setWidth("100%");
+		// create the timeline
+		EventTimeline timeline = new EventTimeline("Our event timeline");
+		timeline.setHeight("500px");
+		timeline.setWidth("100%");
 
-    // set the visible time range
-    Calendar cal = Calendar.getInstance();
-    Date start = cal.getTime();
-    cal.add(Calendar.HOUR_OF_DAY, 24);
-    Date end = cal.getTime();
-    timeline.setVisibleDateRange(start, end);
+		// set the visible time range
+		Calendar cal = Calendar.getInstance();
+		Date start = cal.getTime();
+		cal.add(Calendar.HOUR_OF_DAY, 4);
+		Date end = cal.getTime();
+		timeline.setVisibleDateRange(start, end);
 
-    // add our data sources
-    timeline.addEventBand("Band", createEventProvider(end));
+		// add our data sources
+		timeline.addEventBand("Band", createEventProvider(end));
+		timeline.addEventBand("Band2", createEventProvider2(end));
+		
+		// Add some zoom levels
+		timeline.addZoomLevel("Hour", 60 * 60 * 1000L);
+		timeline.addZoomLevel("Day", 86400000L);
 
-    // Add some zoom levels
-    timeline.addZoomLevel("Hour", 60 * 60 * 1000L);
-    timeline.addZoomLevel("Day", 86400000L);
+		mainWindow.addComponent(timeline);
+	}
 
-    mainWindow.addComponent(timeline);
-  }
+	public TimelineEventProvider createEventProvider(final Date end) {
+		BasicEventProvider provider = new BasicEventProvider();
 
-  public TimelineEventProvider createEventProvider(final Date end) {
-    BasicEventProvider provider = new BasicEventProvider();
+		// get events for a whole day
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.HOUR, 1);
+		int idx = 0;
+		while (cal.getTime().before(end)) {
+			// create a simple event
+			BasicEvent event = new BasicEvent();
+			event.setEventId(String.valueOf(idx));
 
-    // get events for a whole day
-    Calendar cal = Calendar.getInstance();
-    cal.add(Calendar.HOUR, 1);
-    int idx = 0;
-    while (cal.getTime().before(end)) {
-      // create a simple event
-      BasicEvent event = new BasicEvent();
-      event.setEventId(String.valueOf(idx));
-      
-      // set the timestamp property
-      event.setStart(cal.getTime());
-      cal.add(Calendar.MINUTE, 10);
-      event.setEnd(cal.getTime());
-      // set the caption
-      event.setCaption("Event");
-      // style the event
-      event.setStyleName("color1");
+			// set the timestamp property
+			event.setStart(cal.getTime());
+			cal.add(Calendar.MINUTE, 10);
+			event.setEnd(cal.getTime());
+			// set the caption
+			event.setCaption("Event");
+			// style the event
+			event.setStyleName("color1");
 
-      provider.addEvent(event);
-      cal.add(Calendar.MINUTE, 30);      
-    }
+			provider.addEvent(event);
+			cal.add(Calendar.MINUTE, 20);
+		}
 
-    return provider;
-  }  
+		return provider;
+	}
+
+	public TimelineEventProvider createEventProvider2(final Date end) {
+		BasicEventProvider provider = new BasicEventProvider();
+
+		// get events for a whole day
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.HOUR, 1);
+		cal.add(Calendar.MINUTE, 17);
+		int idx = 0;
+		while (cal.getTime().before(end)) {
+			// create a simple event
+			BasicEvent event = new BasicEvent();
+			event.setEventId(String.valueOf(idx));
+
+			// set the timestamp property
+			event.setStart(cal.getTime());
+			cal.add(Calendar.MINUTE, 10);
+			event.setEnd(cal.getTime());
+			// set the caption
+			event.setCaption("Event");
+			// style the event
+			event.setStyleName("color2");
+
+			provider.addEvent(event);
+			cal.add(Calendar.MINUTE, 20);
+		}
+
+		return provider;
+	}
 }
