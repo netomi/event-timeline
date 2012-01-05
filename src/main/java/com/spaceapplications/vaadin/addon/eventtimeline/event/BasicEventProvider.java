6d82fa2baad6b993344b6324f974fe839d5c73f7
Florian Pirchner <florian.pirchner@gmail.com>
2012-01-05 11:48:40 +0100

    getter to return all events.

--- src/main/java/com/spaceapplications/vaadin/addon/eventtimeline/event/BasicEventProvider.java
+++ src/main/java/com/spaceapplications/vaadin/addon/eventtimeline/event/BasicEventProvider.java
@@ -12,109 +12,123 @@ import java.util.TreeSet;
 
 /**
  * <p>
- * Simple implementation of {@link TimelineEventProvider}. Use {@link #addEvent(BasicEvent)} and
- * {@link #removeEvent(BasicEvent)} to add / remove events.
+ * Simple implementation of {@link TimelineEventProvider}. Use
+ * {@link #addEvent(BasicEvent)} and {@link #removeEvent(BasicEvent)} to add /
+ * remove events.
  * </p>
  * 
  * <p>
- * {@link TimelineEventProvider.EventSetChangeNotifier EventSetChangeNotifier} and
- * {@link TimelineEvent.EventChangeListener EventChangeListener} are also implemented, so the
- * Calendar is notified when an event is added, changed or removed.
+ * {@link TimelineEventProvider.EventSetChangeNotifier EventSetChangeNotifier}
+ * and {@link TimelineEvent.EventChangeListener EventChangeListener} are also
+ * implemented, so the Calendar is notified when an event is added, changed or
+ * removed.
  * </p>
  */
 public class BasicEventProvider implements TimelineEventProvider,
-    TimelineEventProvider.EventSetChangeNotifier, TimelineEvent.EventChangeListener {
+		TimelineEventProvider.EventSetChangeNotifier,
+		TimelineEvent.EventChangeListener {
 
-     private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-  protected Set<TimelineEvent> eventList = new TreeSet<TimelineEvent>(new Comparator<TimelineEvent>() {
-    @Override
-    public int compare(TimelineEvent o1, TimelineEvent o2) {
-      int diff = o1.getStart().compareTo(o2.getStart());
-      if (diff != 0) {
-        return diff;
-      } else {
-        diff = o1.getEnd().compareTo(o2.getEnd());
-        if (diff != 0) {
-          return diff;
-        } else {
-          return o1.getEventId().compareTo(o2.getEventId());
-        }
-      }
-    }
-  });
+	protected Set<TimelineEvent> eventList = new TreeSet<TimelineEvent>(
+			new Comparator<TimelineEvent>() {
+				@Override
+				public int compare(TimelineEvent o1, TimelineEvent o2) {
+					int diff = o1.getStart().compareTo(o2.getStart());
+					if (diff != 0) {
+						return diff;
+					} else {
+						diff = o1.getEnd().compareTo(o2.getEnd());
+						if (diff != 0) {
+							return diff;
+						} else {
+							return o1.getEventId().compareTo(o2.getEventId());
+						}
+					}
+				}
+			});
 
-  private List<EventSetChangeListener> listeners = new ArrayList<EventSetChangeListener>();
+	private List<EventSetChangeListener> listeners = new ArrayList<EventSetChangeListener>();
 
-  /**
-   * {@inheritDoc}
-   */
-  public List<TimelineEvent> getEvents(Date startDate, Date endDate) {
-    ArrayList<TimelineEvent> activeEvents = new ArrayList<TimelineEvent>();
+	/**
+	 * {@inheritDoc}
+	 */
+	public List<TimelineEvent> getEvents(Date startDate, Date endDate) {
+		ArrayList<TimelineEvent> activeEvents = new ArrayList<TimelineEvent>();
 
-    for (TimelineEvent ev : eventList) {
-      long from = startDate.getTime();
-      long to = endDate.getTime();
+		for (TimelineEvent ev : eventList) {
+			long from = startDate.getTime();
+			long to = endDate.getTime();
 
-      long f = ev.getStart().getTime();
-      long t = ev.getEnd().getTime();
-      // Select only events that overlaps with startDate and
-      // endDate.
-      if ((f <= to && f >= from) || (t >= from && t <= to) || (f <= from && t >= to)) {
-        activeEvents.add(ev);
-      }
-    }
+			long f = ev.getStart().getTime();
+			long t = ev.getEnd().getTime();
+			// Select only events that overlaps with startDate and
+			// endDate.
+			if ((f <= to && f >= from) || (t >= from && t <= to)
+					|| (f <= from && t >= to)) {
+				activeEvents.add(ev);
+			}
+		}
 
-    return activeEvents;
-  }
+		return activeEvents;
+	}
 
-  public void addEvent(BasicEvent event) {
-    eventList.add(event);
+	/**
+	 * Returns all events and ignores any date ranges.
+	 */
+	public List<TimelineEvent> getEvents() {
+		ArrayList<TimelineEvent> activeEvents = new ArrayList<TimelineEvent>(
+				eventList);
+		return activeEvents;
+	}
 
-    event.addListener(this);
+	public void addEvent(BasicEvent event) {
+		eventList.add(event);
 
-    fireEventSetChange();
-  }
+		event.addListener(this);
 
-  public void removeEvent(BasicEvent event) {
-    eventList.remove(event);
+		fireEventSetChange();
+	}
 
-    event.removeListener(this);
+	public void removeEvent(BasicEvent event) {
+		eventList.remove(event);
 
-    fireEventSetChange();
-  }
+		event.removeListener(this);
 
-  public boolean containsEvent(BasicEvent event) {
-    return eventList.contains(event);
-  }
+		fireEventSetChange();
+	}
 
-  /**
-   * {@inheritDoc}
-   */
-  public void addListener(EventSetChangeListener listener) {
-    listeners.add(listener);
-  }
+	public boolean containsEvent(BasicEvent event) {
+		return eventList.contains(event);
+	}
 
-  /**
-   * {@inheritDoc}
-   */
-  public void removeListener(EventSetChangeListener listener) {
-    listeners.remove(listener);
-  }
+	/**
+	 * {@inheritDoc}
+	 */
+	public void addListener(EventSetChangeListener listener) {
+		listeners.add(listener);
+	}
 
-  protected void fireEventSetChange() {
-    EventSetChange event = new EventSetChange(this);
+	/**
+	 * {@inheritDoc}
+	 */
+	public void removeListener(EventSetChangeListener listener) {
+		listeners.remove(listener);
+	}
 
-    for (EventSetChangeListener listener : listeners) {
-      listener.eventSetChange(event);
-    }
-  }
+	protected void fireEventSetChange() {
+		EventSetChange event = new EventSetChange(this);
 
-  /**
-   * {@inheritDoc}
-   */
-  public void eventChange(TimelineEvent.EventChange changeEvent) {
-    // naive implementation
-    fireEventSetChange();
-  }
+		for (EventSetChangeListener listener : listeners) {
+			listener.eventSetChange(event);
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void eventChange(TimelineEvent.EventChange changeEvent) {
+		// naive implementation
+		fireEventSetChange();
+	}
 }
